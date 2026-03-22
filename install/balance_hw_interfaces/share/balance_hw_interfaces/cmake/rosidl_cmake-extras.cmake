e05df7ffac08c7be00c463fc69945b16031c70d2
Matthew McGraw <46411758+matthew-mcgraw@users.noreply.github.com>
2026-03-22 02:29:19 -0400

    Delete install directory

--- install/balance_hw_interfaces/share/balance_hw_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(balance_hw_interfaces_IDL_FILES "srv/SetDirection.idl;srv/SetPwm.idl")
-set(balance_hw_interfaces_INTERFACE_FILES "srv/SetDirection.srv;srv/SetPwm.srv")
