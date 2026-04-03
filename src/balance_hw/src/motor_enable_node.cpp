faae62c2f640d59f51f08f41a17a7fcfa8a57e95
Matthew McGraw <mcgrawm1415@gmail.com>
2026-04-03 02:17:50 -0700

    .

--- src/balance_hw/src/motor_enable_node.cpp
+++ src/balance_hw/src/motor_enable_node.cpp
@@ -214,7 +214,7 @@ class MotorEnableNode : public rclcpp::Node {
         double pwm_cmd_{0.0};
         rclcpp::Time last_cmd_time_;
         rclcpp::TimerBase::SharedPtr cmd_watchdog_timer_;
-        int cmd_timeout_ms_{250};
+        int cmd_timeout_ms_{10000};
 
         bool set_motor_a(bool enable){
             if (!pwm_a_line_){
@@ -421,8 +421,3 @@ int main(int argc, char** argv){
 
 
 
-/*
-
-
-
-*/
